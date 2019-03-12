37fefe791f98d3c33a582884d3627328ec96b43b
liyun <li_yun@gowild.cn>
2019-03-12 15:00:09 +0800

    调整文字位置

--- src/components/draw-font.js
+++ src/components/draw-font.js
@@ -13,11 +13,11 @@ const initScene = () => {
 let camera
 const initCamera = () => {
     camera = new THREE.PerspectiveCamera( 45, 1, 1, 500 )
-    camera.position.set(130,130,130)
+    camera.position.set(80,20,200)
     camera.up.z = 0
     camera.up.y = 1
     camera.up.x = 0
-    camera.lookAt(new THREE.Vector3(0,0,0));
+    camera.lookAt(new THREE.Vector3(40,40,0));
 }
 
 // 3.渲染器
@@ -43,22 +43,23 @@ const initObject = () => {
 
     let fontLoader = new THREE.FontLoader();
     fontLoader.load('https://threejs.org/examples/fonts/helvetiker_regular.typeface.json',function(font) {
+
       let textGeometry = new THREE.TextGeometry('GOWILD',{
         font: font,
-        size: 18,
-		height: 10,
-		// curveSegments: 12,
-		// bevelEnabled: true,
-		// bevelThickness: 2,
-		// bevelSize: 5
+        size: 20,
+		height: 10
       })
-      var meshMaterial = new THREE.MeshNormalMaterial({
-        flatShading: THREE.FlatShading,
-        transparent: true,
-        opacity: 1
-      })
-    //   const textMesh = new THREE.Points(textGeometry, new THREE.PointsMaterial( { size: 100, color: '0x29b6f6' } ))
-      const textMesh = new THREE.Mesh(textGeometry, meshMaterial)
+
+    //   var meshMaterial = new THREE.MeshNormalMaterial({
+    //     flatShading: THREE.FlatShading,
+    //     transparent: true,
+    //     opacity: 1
+    //   })
+    //   const textMesh = new THREE.Mesh(textGeometry, meshMaterial)
+
+      var meshMaterial = new THREE.PointsMaterial( { color: 0x29b6f6, size: 3,sizeAttenuation: false} )
+      const textMesh = new THREE.Points(textGeometry, meshMaterial)
+
       textMesh.position.set(0, 0, 0)
       scene.add(textMesh)
       // 必须在这里渲染一次，因为字体加载是异步的，否则在字体包还没加载完就渲染了就看不到字体了
